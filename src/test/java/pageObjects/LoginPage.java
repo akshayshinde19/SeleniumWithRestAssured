577164201ca36799ebab00696e9a02c5ddac932a
akshay <shinde190894@gmail.com>
2022-03-12 16:01:37 +0530

    verification changes done

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -2,8 +2,10 @@ package pageObjects;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.junit.Assert;
@@ -20,11 +22,17 @@ import io.restassured.response.Response;
 public class LoginPage {
 
 	public WebDriver ldriver;
-	ArrayList<String> nameListFromAPI = new ArrayList();;
-	ArrayList<String> nameListFromUI = new ArrayList();;
+	//ArrayList<String> nameListFromAPI = new ArrayList();;
+	Map<String,String> nameHSFromAPI = new HashMap<String,String>();
+	
+	//ArrayList<String> nameListFromUI = new ArrayList();;
+	Map<String,String> nameHSFromUI = new HashMap<String,String>();
+	
+	
+	
 
-	ArrayList<String> descListFromAPI = new ArrayList();;
-	ArrayList<String> descListFromUI = new ArrayList();;
+	//ArrayList<String> descListFromAPI = new ArrayList();;
+	//ArrayList<String> descListFromUI = new ArrayList();;
 
 	Response response;
 
@@ -46,6 +54,7 @@ public class LoginPage {
 	List<WebElement> reposDesc;
 	
 	
+	//p[@itemprop='description']//path +repos[i]
 
 	public void clickRepo() {
 		repoTab.click();
@@ -60,34 +69,54 @@ public class LoginPage {
 
 		while (it.hasNext()) {
 			// System.out.println(it.next().getText());
-			nameListFromUI.add(it.next().getText());
+			//nameListFromUI.add(it.next().getText());
+			
+			//a[@itemprop='name codeRepository' and normalize-space() = 'daphne']//parent::*//parent::*//p[@itemprop='description']
+			
+			//By.xpath(null).toString();
+			
+			String t=it.next().getText();
+			String d="";
+			try {
+			 d=ldriver.findElement(By.xpath(
+					"//a[@itemprop='name codeRepository' and normalize-space() = '"+t+"']//parent::*//parent::*//p[@itemprop='description']")).getText();
+			}
+			catch(Exception e) {
+				d="null";
+			}
+			nameHSFromUI.put(t, d);
+			
+			
 		}
 
-		Collections.sort(nameListFromUI);
+		//Collections.sort(nameListFromUI);
 		
 		System.out.println("*********nameListFromUI*********");
+//		nameHSFromUI.put("1", "ak1");
+//		nameHSFromUI.put("2", "ak2");
+//		nameHSFromUI.put("3", "ak3");
 
-		System.out.println(nameListFromUI);
+		System.out.println(nameHSFromUI);
 		
 		
-		Iterator<WebElement> it1 = webListDesc.iterator();
-
-		while (it1.hasNext()) {
-			// System.out.println(it.next().getText());
-//			String temp=it1.next().getText();
-//			if(!temp.equals(null)) {
-			descListFromUI.add(it1.next().getText());
-//			}
-		}
-		
-		
-
-		Collections.sort(descListFromUI);
-		
-		
-		System.out.println("*********descListFromUI*********");
-
-		System.out.println(descListFromUI);
+//		Iterator<WebElement> it1 = webListDesc.iterator();
+//
+//		while (it1.hasNext()) {
+//			// System.out.println(it.next().getText());
+////			String temp=it1.next().getText();
+////			if(!temp.equals(null)) {
+//			descListFromUI.add(it1.next().getText());
+////			}
+//		}
+//		
+//		
+//
+//		Collections.sort(descListFromUI);
+//		
+//		
+//		System.out.println("*********descListFromUI*********");
+//
+//		System.out.println(descListFromUI);
 
 	}
 
@@ -112,8 +141,12 @@ public class LoginPage {
 		while (i < JSONResponseBody.length()) {
 			
 
-			nameListFromAPI.add((JSONResponseBody.getJSONObject(i).getString("name")));
-			descListFromAPI.add((JSONResponseBody.getJSONObject(i).get("description").toString()));
+//			nameListFromAPI.add((JSONResponseBody.getJSONObject(i).getString("name")));
+//			descListFromAPI.add((JSONResponseBody.getJSONObject(i).get("description").toString()));
+//			
+			
+			nameHSFromAPI.put((JSONResponseBody.getJSONObject(i).getString("name")), (JSONResponseBody.getJSONObject(i).get("description").toString()));
+			
 			i++;
 		}
 
@@ -121,21 +154,54 @@ public class LoginPage {
 //			System.out.println(nameListFromAPI.get(j));
 //		}
 
-		Collections.sort(nameListFromAPI);
-		Collections.sort(descListFromAPI);
+		//Collections.sort(nameListFromAPI);
+		//Collections.sort(descListFromAPI);
 		
 		//while(descListFromAPI.remove(null));
 		
-		descListFromAPI.removeAll(Collections.singleton("null"));
+		//descListFromAPI.removeAll(Collections.singleton("null"));
 
-		System.out.println(nameListFromAPI);
-		System.out.println(descListFromAPI);
+		//System.out.println(nameListFromAPI);
+		
+		
+//		nameHSFromAPI.put("1", "ak1");
+//		nameHSFromAPI.put("3", "ak9");
+//		nameHSFromAPI.put("2", "ak2");
+		
+		System.out.println("api hs === "+nameHSFromAPI);
 
 	}
 
 	public void verifyTitlesAndDescFromUIandAPI() {
-		Assert.assertEquals(nameListFromAPI, nameListFromUI);
-		Assert.assertEquals(descListFromAPI, descListFromUI);
+//		Assert.assertEquals(nameListFromAPI, nameListFromUI);
+//		Assert.assertEquals(descListFromAPI, descListFromUI);
+		
+//		 Iterator itUI = nameHSFromUI.entrySet().iterator();
+//		    while (itUI.hasNext()) {
+//		        Map.Entry pair = (Map.Entry)itUI.next();
+//		        System.out.println("This is UI hm : "+pair.getKey() + " = " + pair.getValue());
+//		        Iterator itAPI = nameHSFromAPI.entrySet().iterator();
+//		        while (itAPI.hasNext()) {
+//			        Map.Entry pairAPI = (Map.Entry)itAPI.next();
+//			        System.out.println("This is API hm : "+pairAPI.getKey() + " = " + pairAPI.getValue());
+//			        if(pair.getKey()==pairAPI.getKey() && pair.getValue()==pairAPI.getValue()) {
+//			        	System.out.println("good");
+//			        	break;
+//			        }
+//			       
+//			    }
+//		       
+//		    }
+		
+		System.out.println("Validation");
+		
+		if(nameHSFromUI.equals(nameHSFromAPI)) {
+			System.out.println("passed");
+		}
+		else {
+			System.out.println("failed");
+		}
+		
 
 	}
 
